e827fc0b24dc729263c856eb3da53be64d002e40
Bram Mittendorff <b.mittendorff@nerds.company>
2016-09-15 11:40:47 +0200

    Added a directory scan option

--- Scan.py
+++ Scan.py
@@ -6,6 +6,7 @@ parser = argparse.ArgumentParser(description='A simple security scanner.')
 parser.add_argument('--url', help='a url to scan', type=str)
 parser.add_argument('--file', help='a url file list to scan', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
 parser.add_argument('--all', help='the mother of all scans', action='store_true')
+parser.add_argument('--directories', help='only scan the headers', action='store_true')
 parser.add_argument('--headers', help='only scan the headers', action='store_true')
 parser.add_argument('--dns', help='only scan DNS', action='store_true')
 args = parser.parse_args()
@@ -13,6 +14,8 @@ args = parser.parse_args()
 if args.url:
     scanner = SecurityScanner.SecurityScanner()
     scanner.addUrl(args.url)
+    if args.all or args.directories:
+        scanner.searchDirectories()
     if args.all or args.headers:
         scanner.searchHeaders()
     if args.all or args.dns:
@@ -20,6 +23,8 @@ if args.url:
 elif args.file is not sys.stdin:
     scanner = SecurityScanner.SecurityScanner()
     scanner.addFile(args.file)
+    if args.all or args.directories:
+        scanner.searchDirectories()
     if args.all or args.headers:
         scanner.searchHeaders()
     if args.all or args.dns:
--- SecurityScanner.py
+++ SecurityScanner.py
@@ -1,7 +1,13 @@
 import requests
 import sys
+import os
 import Queue
 import threading
+
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
 
 class SecurityScanner:
 
@@ -14,10 +20,10 @@ class SecurityScanner:
         self.queue = None
         self.session = requests.Session()
 
-    def runRequests(self, function, listRequests):
+    def runRequests(self, runFunction, listRequests):
         self.queue = Queue.Queue(self.concurrent * 2)
         # call method by name in string
-        method = getattr(SecurityScanner, function)
+        method = getattr(SecurityScanner, runFunction)
         # unbound to bound method
         boundMethod = method.__get__(self, SecurityScanner)
         for _ in range(self.concurrent):
@@ -27,7 +33,7 @@ class SecurityScanner:
         try:
             for request in listRequests:
                 self.queue.put(request)
-                self.queue.join()
+            self.queue.join()
         except KeyboardInterrupt:
             sys.exit(1)
 
@@ -41,9 +47,25 @@ class SecurityScanner:
         for url in fileLocation:
             self.urls.append(url)
 
+    def searchDirectories(self):
+        directoryRequests = []
+        directoryFileName = 'resources/directories.txt'
+        if(os.path.isfile(directoryFileName)):
+            for url in self.urls:
+                with open(directoryFileName) as directoryFile:
+                    for filename in directoryFile:
+                        directoryRequests.append(url + '/' + filename)
+            self.runRequests('resultDirectories', directoryRequests)
+
+    def resultDirectories(self):
+        while True:
+            url = self.queue.get()
+            request = self.session.get(url)
+            print("%s on url: %s" % (request.status_code, url.rstrip()))
+            self.queue.task_done()
+
     def searchHeaders(self):
-        headerRequests = []
-        self.runRequests('resultHeaders', headerRequests)
+        self.runRequests('resultHeaders', self.urls)
 
     def resultHeaders(self):
         url = self.queue.get()
@@ -51,9 +73,10 @@ class SecurityScanner:
         print(request.headers)
         self.queue.task_done()
 
-
     def searchDNS(self):
         dnsRequests = []
+        for url in self.urls:
+            print(url)
         self.runRequests('resultDNS', dnsRequests)
 
     def resultDNS(self):
